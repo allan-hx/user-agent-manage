9f0d9fc8a7c488266595078631397feac2df297e
allan-hx <allan_qvq@163.com>
2025-05-01 16:22:49 +0800

    feat(Add List Management)

--- src/layout/app.tsx
+++ src/layout/app.tsx
@@ -1,7 +1,8 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { getHostname } from '@/lib/utils';
-import { Save, Trash2 } from 'lucide-react';
+import { Cat, Save, Trash2 } from 'lucide-react';
 import React, {
   useCallback,
   useEffect,
@@ -9,7 +10,18 @@ import React, {
   type ChangeEvent,
 } from 'react';
 
+interface Item {
+  // hostname
+  key: string;
+  // ua
+  value: string;
+}
+
 export default React.memo(function App() {
+  // 当前hostname
+  const [hostname, setHostname] = useState('');
+  // 全部设置数据
+  const [data, setData] = useState<Item[]>([]);
   // 当前输入的值
   const [value, setValue] = useState('');
   // 是否存在User Agent
@@ -22,52 +34,104 @@ export default React.memo(function App() {
   }, []);
 
   const onSave = useCallback(async () => {
-    const origin = await getHostname();
     await chrome.storage.local.set({
-      [origin]: value,
+      [hostname]: value,
     });
     setExist(true);
+    setData([{ key: hostname, value }, ...data]);
     updateDynamicRules();
-  }, [value, updateDynamicRules]);
+  }, [hostname, value, data, updateDynamicRules]);
 
-  const onRemove = useCallback(async () => {
-    const origin = await getHostname();
-    await chrome.storage.local.remove(origin);
-    setExist(false);
-    setValue('');
-    updateDynamicRules();
-  }, [updateDynamicRules]);
+  const onRemove = useCallback(
+    async (key: string) => {
+      await chrome.storage.local.remove(key);
+      setData(data.filter((item) => item.key !== key));
+      updateDynamicRules();
+    },
+    [data, updateDynamicRules],
+  );
 
   const onInput = useCallback((event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value.trim());
   }, []);
 
-  useEffect(() => {
-    (async () => {
-      const origin = await getHostname();
-      const result = await chrome.storage.local.get(origin);
+  const onRemoveCurrent = useCallback(async () => {
+    await onRemove(hostname);
+    setValue('');
+    setExist(false);
+  }, [hostname, onRemove]);
 
-      if (origin in result) {
-        setExist(true);
-        setValue(result[origin]);
-      }
-    })();
+  const init = useCallback(async () => {
+    const hostname = await getHostname();
+    const result = await chrome.storage.local.get();
+
+    if (hostname in result) {
+      setExist(true);
+      setValue(result[hostname]);
+    }
+
+    const data = Object.keys(result).map((key) => ({
+      key,
+      value: result[key],
+    }));
+
+    setData(data);
+    setHostname(hostname);
   }, []);
 
+  useEffect(() => {
+    init();
+  }, [init]);
+
   return (
-    <div className="w-[360px] h-[500px] flex flex-col mx-auto bg-background">
-      <ul className="flex flex-col flex-1 bg-red-300" />
-      <div className="flex gap-2 p-4">
+    <div className="w-[360px] h-[500px] py-3 flex flex-col mx-auto bg-background">
+      {data.length ? (
+        <ScrollArea className="flex-1 min-h-0 px-3">
+          <div className="flex flex-col gap-3">
+            {data.map((item) => (
+              <div
+                key={item.key}
+                className="flex items-center border border-dashed p-3 rounded-sm hover:border-[var(--primary)] hover:bg-muted/50"
+              >
+                <div className="flex-1">
+                  <div className="text-[12px] font-extrabold mb-0.5">
+                    hostname: {item.key}
+                  </div>
+                  <div className="text-[12px]">ua: {item.value}</div>
+                </div>
+                <Trash2
+                  className="size-[17px] cursor-pointer hover:opacity-60 hover:text-destructive"
+                  onClick={() =>
+                    item.key === hostname
+                      ? onRemoveCurrent()
+                      : onRemove(item.key)
+                  }
+                />
+              </div>
+            ))}
+          </div>
+        </ScrollArea>
+      ) : (
+        <div className="flex-1 min-h-0 flex items-center justify-center">
+          <Cat
+            color="var(--muted-foreground)"
+            className="size-28 opacity-20"
+            strokeWidth={1}
+          />
+        </div>
+      )}
+      <div className="flex gap-2 p-3">
         <Input
           autoFocus
-          className="text-[14px]"
-          placeholder="Please enter the User Agent string"
+          className="text-[14px] border-dashed"
+          placeholder="Please enter the User agent"
           value={value}
           onInput={onInput}
         />
         <Button
           variant="outline"
           size="icon"
+          className="border-dashed"
           disabled={!value.length}
           onClick={onSave}
         >
@@ -76,8 +140,9 @@ export default React.memo(function App() {
         <Button
           variant="outline"
           size="icon"
+          className="border-dashed"
           disabled={!isExist}
-          onClick={onRemove}
+          onClick={onRemoveCurrent}
         >
           <Trash2 />
         </Button>
